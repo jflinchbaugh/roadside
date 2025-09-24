e90e8c0d4886088b17be6ed3f7f3fc06afd6d2c9
John Flinchbaugh <john@hjsoft.com>
2025-09-24 00:08:34 -0400

    trim out old stuff

--- server/src/server/core.clj
+++ server/src/server/core.clj
@@ -39,10 +39,6 @@
   [_]
   (api-response 200 "pong"))
 
-(defn get-logger
-  [req]
-  (get-in req [:path-params :id]))
-
 (defn download-handler
   [req]
   (if-not (:identity req)
@@ -50,17 +46,7 @@
     (let [logger (get-logger req)]
       (api-response
        200
-       (get-in @storage [logger :document])))))
-
-(defn save-logger!
-  [id document]
-  (swap!
-   storage
-   (fn [s]
-     (->
-      s
-      (assoc-in [id :document] document))))
-  (tel/log! :info (format "Saved document for '%s'" id)))
+       nil))))
 
 (defn upload-handler
   [req]
@@ -68,26 +54,9 @@
     (not-found)
     (let [request-body (rur/body-string req)
           id (get-logger req)]
-      (save-logger! id request-body)
       (api-response
        200
        request-body))))
-
-(defn register-logger!
-  [id login password]
-  (swap!
-   storage
-   assoc
-   id
-   {:login login
-    :password password
-    :document nil})
-  (tel/log! :info (format "Registered '%s' for '%s'" id login)))
-
-(defn unregister-logger!
-  [id]
-  (swap! storage dissoc id)
-  (tel/log! :info (format "Unregistered '%s'" id)))
 
 (defn owner?
   [login logger]
@@ -102,20 +71,17 @@
     (if (get @storage id)
       (api-response 200 (format "'%s' already exists" id))
       (do
-        (register-logger! id login password)
         (api-response
           200
           (format "'%s' created. Access it as '%s'." id resource))))))
 
-(defn unregister-handler
+(defn delete-handler
   [req]
-  (let [id (get-logger req)
-        logger (get @storage id)
+  (let [id nil
         login (:identity req)]
     (if-not (and logger (owner? login logger))
       (not-found)
       (do
-        (unregister-logger! id)
         (api-response 200 (format "'%s' deleted" id))))))
 
 (defn identity-required-wrapper
@@ -128,10 +94,8 @@
 (defn my-authfn
   [req authdata]
   (let [login (:username authdata)
-        password (:password authdata)
-        id (get-logger req)
-        existing-logger (get @storage id)]
-    (when (= ((juxt :login :password) existing-logger) [login password])
+        password (:password authdata)]
+    (when (= [login password] [login password])
       login)))
 
 (def backend (backends/basic {:realm realm :authfn my-authfn}))
@@ -144,7 +108,7 @@
        ["/api"
         ["/ping" ping-handler]
         ["/register" {:post register-handler}]
-        ["/document/:id" {:middleware
+        ["/stands/:id" {:middleware
                         [authenticated-for-logger identity-required-wrapper]
                         :get download-handler
                         :post upload-handler
@@ -161,34 +125,6 @@
        (assoc
         rmd/api-defaults
         :proxy true))))
-
-(defn connect-db
-  "wire storage atom into xtdb"
-  [db-host]
-  (remove-watch storage :to-xtdb)
-
-  (let [node (xt/client {:host db-host})]
-    (->>
-     (xt/q node '(from :roadside [_id document login password]))
-     (reduce
-      (fn [store doc] (assoc store (:xt/id doc) (dissoc doc :xt/id)))
-      {})
-     (reset! storage)))
-
-  (add-watch storage :to-xtdb
-             (fn [_name _atom old-val new-val]
-               (let [old-keys (keys old-val)
-                     new-keys (keys new-val)
-                     removed (remove (set new-keys) old-keys)
-                     node (xt/client {:host db-host})]
-                 (xt/submit-tx
-                  node
-                  (concat
-                   (for [id removed]
-                     [:delete-docs :roadside id])
-                   (for [doc new-val]
-                     [:put-docs :roadside
-                      (merge {:xt/id (first doc)} (second doc))])))))))
 
 (defonce server (atom nil))
 
@@ -218,13 +154,7 @@
 
   (stop-server!)
 
-  (app {:scheme :http :request-method :get :uri "/document/api/logger/x"})
-
-  (app {:scheme :http :request-method :get :uri "/document/api/logger/events"})
-
-  (app {:scheme :http :request-method :get :uri "/document/api/ping"})
-
-  @storage
+  (app {:scheme :http :request-method :get :uri "/roadside/api/ping"})
 
   (let [node (xt/client {:host "localhost"})]
     (xt/q node '(from :roadside [*])))
