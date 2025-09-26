6bf5719d6ca3be93a4c20bfada6a9daa8c98ecc3
John Flinchbaugh <john@hjsoft.com>
2025-09-26 01:34:54 -0400

    no need for "testing" forms

--- website/src/test/com/hjsoft/roadside/website/core_test.cljs
+++ website/src/test/com/hjsoft/roadside/website/core_test.cljs
@@ -2,34 +2,28 @@
   (:require [com.hjsoft.roadside.website.core :as sut]
             [cljs.test :as t]))
 
-(t/testing "things"
-  (t/deftest example-test
-    (t/is true)))
+(t/deftest in-a-week
+  (t/is (re-matches #"\d{4}-\d{2}-\d{2}" (sut/in-a-week))
+    "iso date format")
+  (t/is (not= (js/Date.) (js/Date. (sut/in-a-week)))
+    "it's not now")
+  (t/is (< (js/Date.) (js/Date. (sut/in-a-week)))
+    "it's in the future")
+  (t/is (= 7
+          (- (int (/ (.getTime (js/Date. (sut/in-a-week))) 1000 60 60 24))
+              (int (/ (.getTime (js/Date.)) 1000 60 60 24))))
+    "7 days into the future"))
 
-(t/testing "in-a-week"
-  (t/deftest in-a-week
-    (t/is (re-matches #"\d{4}-\d{2}-\d{2}" (sut/in-a-week))
-      "iso date format")
-    (t/is (not= (js/Date.) (js/Date. (sut/in-a-week)))
-      "it's not now")
-    (t/is (< (js/Date.) (js/Date. (sut/in-a-week)))
-      "it's in the future")
-    (t/is (= 7
-            (- (int (/ (.getTime (js/Date. (sut/in-a-week))) 1000 60 60 24))
-               (int (/ (.getTime (js/Date.)) 1000 60 60 24))))
-      "7 days into the future")))
-
-(t/testing "stand-key"
-  (t/deftest stand-key
-    (t/is (= "-----" (sut/stand-key nil))
-      "nil key")
-    (t/is (= "-----" (sut/stand-key {}))
-      "empty key")
-    (t/is (= "name-1-2-address-town-state-prod-thing"
-            (sut/stand-key {:name "name"
-                            :coordinate [1.0 2.0]
-                            :address "address"
-                            :town "town"
-                            :state "state"
-                            :products ["prod" "thing"]}))
-      "stand key built from fields")))
+(t/deftest stand-key
+  (t/is (= "-----" (sut/stand-key nil))
+    "nil key")
+  (t/is (= "-----" (sut/stand-key {}))
+    "empty key")
+  (t/is (= "name-1-2-address-town-state-prod-thing"
+          (sut/stand-key {:name "name"
+                          :coordinate [1.0 2.0]
+                          :address "address"
+                          :town "town"
+                          :state "state"
+                          :products ["prod" "thing"]}))
+    "stand key built from fields"))
