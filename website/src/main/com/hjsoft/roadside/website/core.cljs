18e961f8f0e8ec728e82e179a50569c27bd55cbf
John Flinchbaugh <john@hjsoft.com>
2026-01-04 01:25:50 -0500

    whitespace

--- website/src/main/com/hjsoft/roadside/website/core.cljs
+++ website/src/main/com/hjsoft/roadside/website/core.cljs
@@ -813,7 +813,6 @@
                   #(set-notification nil)
                   3000)]
        (fn [] (js/clearTimeout timer)))))
-
   (when notification
     (d/div
      {:class (str "notification-toast " (name (:type notification)))}
