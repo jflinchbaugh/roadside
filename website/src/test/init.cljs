12d714f3842c199592ec3007e25d43d462f5e2e6
John Flinchbaugh <john@hjsoft.com>
2026-02-08 11:17:38 -0500

    init for node tests

---
+++ website/src/test/init.cljs
@@ -0,0 +1,14 @@
+(ns init
+  (:require ["global-jsdom" :as global-jsdom]))
+
+;; Initialize JSDOM immediately so that browser globals are available
+;; during namespace loading of other files.
+(global-jsdom)
+
+(set! js/global.IS_REACT_ACT_ENVIRONMENT true)
+
+;; Mock localStorage if it's not provided by the environment
+(when (cljs.core/undefined? js/localStorage)
+  (set! js/localStorage #js {:getItem (fn [_] nil)
+                             :setItem (fn [_ _] nil)
+                             :removeItem (fn [_] nil)}))
