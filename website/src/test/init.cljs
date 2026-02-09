606e90142f588bfe5ef9fae654b3aba63f2ee818
John Flinchbaugh <john@hjsoft.com>
2026-02-09 09:07:51 -0500

    no need to mock localstorage

--- website/src/test/init.cljs
+++ website/src/test/init.cljs
@@ -4,9 +4,3 @@
 ;; Initialize JSDOM immediately so that browser globals are available
 ;; during namespace loading of other files.
 (global-jsdom)
-
-;; Mock localStorage if it's not provided by the environment
-(when (cljs.core/undefined? js/localStorage)
-  (set! js/localStorage #js {:getItem (fn [_] nil)
-                             :setItem (fn [_ _] nil)
-                             :removeItem (fn [_] nil)}))
