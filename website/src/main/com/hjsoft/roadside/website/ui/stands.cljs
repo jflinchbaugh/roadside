54048e60c39dbbd836c21de2a33dfaa610ab5148
John Flinchbaugh <john@hjsoft.com>
2026-02-08 19:55:24 -0500

    extract a stand-item component

--- website/src/main/com/hjsoft/roadside/website/ui/stands.cljs
+++ website/src/main/com/hjsoft/roadside/website/ui/stands.cljs
@@ -1,10 +1,86 @@
 (ns com.hjsoft.roadside.website.ui.stands
-  (:require [helix.core :refer [defnc]]
+  (:require [helix.core :refer [defnc $]]
             [helix.hooks :as hooks]
             [helix.dom :as d]
             [com.hjsoft.roadside.website.utils :as utils]
             [com.hjsoft.roadside.website.state :as state]
             [clojure.string :as str]))
+
+(defnc stand-item
+  [{:keys [stand selected? on-click on-edit on-delete item-ref]}]
+  (d/div
+   {:key (utils/stand-key stand)
+    :ref item-ref
+    :class (str
+            "stand-item"
+            (when selected? " selected-stand"))
+    :onClick on-click}
+   (d/div
+    {:class "stand-content"}
+    (when (seq (:name stand))
+      (d/div
+       {:class "stand-header"}
+       (d/h4 (:name stand))))
+    (when (seq (:coordinate stand))
+      (d/p {:class "coordinate-text"} (:coordinate stand)))
+    (when (seq (:address stand))
+      (d/p (:address stand)))
+    (let [town-state (remove empty? [(:town stand) (:state stand)])]
+      (when (seq town-state)
+        (d/p (str/join ", " town-state))))
+    (when (seq (:products stand))
+      (d/div
+       {:class "stand-products"}
+       (d/strong "Products: ")
+       (d/div
+        {:class "products-tags"}
+        (map (fn [product]
+               (d/span
+                {:key product
+                 :class "product-tag"}
+                product))
+             (filter string? (:products stand))))))
+    (when (seq (:notes stand))
+      (d/p
+       {:class "stand-notes"}
+       (d/strong "Notes: ")
+       (:notes stand)))
+    (when (seq (:expiration stand))
+      (d/p
+       {:class "expiration-date"}
+       (d/strong "Expires: ")
+       (:expiration stand)))
+    (when (seq (:updated stand))
+      (d/p
+       {:class "stand-updated"}
+       (d/strong "Last Updated: ")
+       (:updated stand)))
+    (d/p
+     {:class "stand-shared"}
+     (d/strong "Shared: ")
+     (if (:shared? stand) "Yes" "No")))
+   (d/div
+    {:class "stand-actions"}
+    (when-let [map-link (utils/make-map-link (:coordinate stand))]
+      (d/a {:href map-link
+            :target "_blank"
+            :rel "noopener noreferrer"
+            :class "go-stand-btn"}
+           "Go"))
+    (d/button
+     {:class "edit-stand-btn"
+      :onClick (fn [e]
+                 (.stopPropagation e)
+                 (on-edit stand))
+      :title "Edit this stand"}
+     "Edit")
+    (d/button
+     {:class "delete-stand-btn"
+      :onClick (fn [e]
+                 (.stopPropagation e)
+                 (on-delete stand))
+      :title "Delete this stand"}
+     "Delete"))))
 
 (defnc stands-list
   [{:keys [stands]}]
@@ -27,78 +103,17 @@
        (d/div
         (map
          (fn [stand]
-           (d/div
-            {:key (utils/stand-key stand)
-             :ref (fn [el] (swap! stand-refs assoc (utils/stand-key stand) el))
-             :class (str
-                     "stand-item"
-                     (when (= (utils/stand-key stand) (utils/stand-key selected-stand))
-                       " selected-stand"))
-             :onClick #(dispatch [:set-selected-stand stand])}
-            (d/div
-             {:class "stand-content"}
-             (when (seq (:name stand))
-               (d/div
-                {:class "stand-header"}
-                (d/h4 (:name stand))))
-             (when (seq (:coordinate stand))
-               (d/p {:class "coordinate-text"} (:coordinate stand)))
-             (when (seq (:address stand))
-               (d/p (:address stand)))
-             (let [town-state (remove empty? [(:town stand) (:state stand)])]
-               (when (seq town-state)
-                 (d/p (str/join ", " town-state))))
-             (when (seq (:products stand))
-               (d/div
-                {:class "stand-products"}
-                (d/strong "Products: ")
-                (d/div
-                 {:class "products-tags"}
-                 (map (fn [product]
-                        (d/span
-                         {:key product
-                          :class "product-tag"}
-                         product))
-                      (filter string? (:products stand))))))
-             (when (seq (:notes stand))
-               (d/p
-                {:class "stand-notes"}
-                (d/strong "Notes: ")
-                (:notes stand)))
-             (when (seq (:expiration stand))
-               (d/p
-                {:class "expiration-date"}
-                (d/strong "Expires: ")
-                (:expiration stand)))
-             (when (seq (:updated stand))
-               (d/p
-                {:class "stand-updated"}
-                (d/strong "Last Updated: ")
-                (:updated stand)))
-             (d/p
-              {:class "stand-shared"}
-              (d/strong "Shared: ")
-              (if (:shared? stand) "Yes" "No")))
-            (d/div
-             {:class "stand-actions"}
-             (when-let [map-link (utils/make-map-link (:coordinate stand))]
-               (d/a {:href map-link
-                     :target "_blank"
-                     :rel "noopener noreferrer"
-                     :class "go-stand-btn"}
-                    "Go"))
-             (d/button
-              {:class "edit-stand-btn"
-               :onClick #(do
-                           (set-editing-stand stand)
+           (let [key (utils/stand-key stand)]
+             ($ stand-item
+              {:key key
+               :stand stand
+               :selected? (= key (utils/stand-key selected-stand))
+               :on-click #(dispatch [:set-selected-stand stand])
+               :on-edit #(do
+                           (set-editing-stand %)
                            (set-show-form true))
-               :title "Edit this stand"}
-              "Edit")
-             (d/button
-              {:class "delete-stand-btn"
-               :onClick #(dispatch [:remove-stand stand])
-               :title "Delete this stand"}
-              "Delete"))))
+               :on-delete #(dispatch [:remove-stand %])
+               :item-ref (fn [el] (swap! stand-refs assoc key el))})))
          stands))))))
 
 (defnc product-list
