4a4ae72850ab47745a0688e1dae63146adc0f578
John Flinchbaugh <john@hjsoft.com>
2026-02-19 23:47:07 -0500

    keep the xtdb container around for cider dev

--- server/test/server/xtdb_container.clj
+++ server/test/server/xtdb_container.clj
@@ -2,18 +2,22 @@
   (:require [clj-test-containers.core :as tc]
             [xtdb.api :as xt]))
 
-(def xtdb-container
-  (tc/create {:image-name "ghcr.io/xtdb/xtdb:2.1.0"
-              :exposed-ports [5432]
-              :wait-for {:strategy :log
-                         :message "XTDB started"}}))
+(defonce xtdb-container
+  (delay
+    (tc/start!
+      (tc/create {:image-name "ghcr.io/xtdb/xtdb:2.1.0"
+                  :exposed-ports [5432]
+                  :wait-for {:strategy :log
+                             :message "XTDB started"}}))))
 
 (defn with-xtdb-client [f]
-  (let [started-container (tc/start! xtdb-container)
+  (let [started-container @xtdb-container
         port (get (:mapped-ports started-container) 5432)
         host (:host started-container)
         node (xt/client {:host host :port port})]
-    (try
-      (f node)
-      (finally
-        (tc/stop! started-container)))))
+    ;; Clear tables for isolation
+    (doseq [table [:users :stands]]
+      (let [docs (xt/q node (list 'from table '[xt/id]))]
+        (when (seq docs)
+          (xt/execute-tx node (mapv (fn [d] [:delete-docs table (:xt/id d)]) docs)))))
+    (f node)))
