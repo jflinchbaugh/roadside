16c08595d688a9b99cdc21f4650fb07d66274ba3
John Flinchbaugh <john@hjsoft.com>
2026-03-20 20:28:46 -0400

    coord is smaller, uneditable, and focus products

--- website/src/main/com/hjsoft/roadside/website/ui/forms/inputs.cljs
+++ website/src/main/com/hjsoft/roadside/website/ui/forms/inputs.cljs
@@ -19,20 +19,7 @@
         stands (state/select-stands-by-expiry app-state)
         map-zoom (:map-zoom app-state 11)
         final-zoom (max add-zoom-level map-zoom)
-        {:keys [get-location error]} (state/use-user-location-state)
-        [coordinate-display
-         set-coordinate-display] (hooks/use-state
-                                  (:coordinate stand-form-data))
-        coordinate-input-ref (hooks/use-ref nil)]
-
-    (hooks/use-effect
-     :once
-     (when-let [el @coordinate-input-ref]
-       (.focus el)))
-
-    (hooks/use-effect
-     [(:coordinate stand-form-data)]
-     (set-coordinate-display (:coordinate stand-form-data)))
+        {:keys [get-location error]} (state/use-user-location-state)]
 
     (d/div
      {:class "form-group"}
@@ -52,13 +39,10 @@
      (d/label "Coordinate:")
      (d/div
       {:class "coordinate-input-group"}
-      (d/input
-       {:type "text"
-        :ref coordinate-input-ref
-        :value coordinate-display
-        :onChange #(set-coordinate-display (.. % -target -value))
-        :onBlur #(on-update [:update-field [:coordinate coordinate-display]])
-        :class "coordinate-input"})
+      (d/span
+       {:class "coordinate-text"
+        :style {:flex-grow 1}}
+       (:coordinate stand-form-data))
       (when original-coordinate
         (d/button
          {:type "button"
@@ -83,24 +67,15 @@
 (defnc product-input [{:keys [stand-form-data on-update]}]
   (let [product-input-ref (hooks/use-ref nil)
         current-product (:current-product stand-form-data "")]
+    (hooks/use-effect
+     :once
+     (when-let [el @product-input-ref]
+       (.focus el)))
     (d/div
      {:class "product-section-wrapper"}
      (d/div
       {:class "form-group"}
       (d/label "Products:")
-      (d/div
-       {:class "products-tags"}
-       (map (fn [product]
-              (d/span
-               {:key product
-                :class "product-tag"}
-               product
-               (d/button
-                {:type "button"
-                 :class "remove-tag"
-                 :onClick #(on-update [:remove-product product])}
-                "\u2715")))
-            (filter string? (:products stand-form-data))))
       (d/div
        {:class "product-input-group"}
        (d/input
@@ -120,4 +95,17 @@
          :onClick (fn []
                     (on-update [:add-product])
                     (when-let [el @product-input-ref] (.focus el)))}
-        "Add"))))))
+        "Add"))
+      (d/div
+       {:class "products-tags"}
+       (map (fn [product]
+              (d/span
+               {:key product
+                :class "product-tag"}
+               product
+               (d/button
+                {:type "button"
+                 :class "remove-tag"
+                 :onClick #(on-update [:remove-product product])}
+                "\u2715")))
+            (filter string? (:products stand-form-data))))))))
